cb3c0a79fae264e73e9ec888fdde6ef1a19b8329
Tam-Pham <vtdn21599@gmail.com>
2026-05-12 15:54:03 +0700

    :+1: Update thumbnail

--- data/products.js
+++ data/products.js
@@ -1,62 +1,62 @@
 const seedProducts = [
   {
     name: 'Cá Bò Khô',
-    price: 320000,
-    unit: 'kg',
+    price: 160000,
+    unit: '500g',
     category: 'dried',
     image: '/images/Cá bò - Gemini.webp',
     isHot: false
   },
   {
     name: 'Cá Chỉ Vàng Khô Ớt',
-    price: 320000,
-    unit: 'kg',
+    price: 170000,
+    unit: '500g',
     category: 'dried',
     image: '/images/Cá chỉ vàng - Gemini.webp',
     isHot: true
   },
   {
     name: 'Cá Đét Khô',
-    price: 400000,
-    unit: 'kg',
+    price: 180000,
+    unit: '500g',
     category: 'dried',
     image: '/images/Cá đét khô -Gemini.webp',
     isHot: false
   },
   {
     name: 'Mực Khô',
-    price: 1600000,
-    unit: 'kg',
+    price: 800000,
+    unit: '500g',
     category: 'dried',
     image: '/images/Mực khô - Gemini.webp',
     isHot: true
   },
   {
     name: 'Mực Tươi',
-    price: 1600000,
-    unit: 'kg',
+    price: 200000,
+    unit: '500g',
     category: 'fresh',
     image: '/images/Mực tươi - Gemini.webp',
     isHot: true
   },
   {
     name: 'Cá Bớp Tươi',
-    price: 1600000,
-    unit: 'kg',
+    price: 130000,
+    unit: '500g',
     category: 'fresh',
     image: '/images/Cá bớp tươi - Gemini.webp',
     isHot: true
   },
     {
     name: 'Hàu Tươi',
-    price: 1600000,
+    price: 70000,
     unit: 'kg',
     category: 'fresh',
     image: '/images/Hàu tươi - Gemini.webp',
     isHot: true
   },    {
     name: 'Nghêu Tươi',
-    price: 1600000,
+    price: 120000,
     unit: 'kg',
     category: 'fresh',
     image: '/images/Nghêu tươi - Gemini.webp',
@@ -64,15 +64,15 @@ const seedProducts = [
   },
    {
     name: 'Ốc Hương Tươi',
-    price: 1600000,
-    unit: 'kg',
+    price: 450000,
+    unit: '500g',
     category: 'fresh',
     image: '/images/Ốc hương tươi - Gemini.webp',
     isHot: true
   },
   {
     name: 'Tôm Tươi',
-    price: 1600000,
+    price: 280000,
     unit: 'kg',
     category: 'fresh',
     image: '/images/Tôm tươi - Gemini.webp',
